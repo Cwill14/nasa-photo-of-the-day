134d493bc592b68ffa5c002a1c953b31e9f50fc5
Clark Williams <theclarkwilliams@gmail.com>
2019-07-11 14:34:25 -0600

    added some styled components, looking for a way to play reverse hover animation when leaving hover state(might need to use JS logic), the photo.url just fails to load, maybe need new key?

--- src/components/NasaPictures.js
+++ src/components/NasaPictures.js
@@ -1,19 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled, { keyframes } from 'styled-components';
 
 const NasaPictures = () => {
     const [photos, setPhotos] = useState([]);
 
+    const grow = keyframes`
+        from {
+            color: white;
+            transform: scale(1);
+        }
+        to {
+            transform: scale(1.25);
+            color: #ddd;
+        }
+    `;
+    const ContainerDiv = styled.div`
+        background-color: #333;
+        padding: 2rem;
+        border-radius: 1rem;
+        width: 66%;
+        margin: 0 auto;
+    `;
+    const Title = styled.h1`
+        font-size: 2.5rem;
+        width: 61%;
+        margin: 0 auto;
+        padding: 0;
+        &:hover {
+            animation: ${grow} 0.75s ease-in;
+            animation-fill-mode: forwards;
+        }
+    `;
+    const Explanation = styled.p`
+        background-color: #222;
+        padding: 2rem;
+        border-radius: 2rem;
+    `;
+    const Italic = styled.div`
+        font-style: italic;
+    `;
+    const PhotoTitle = styled(Italic)`
+        text-shadow: 0 0.33rem black;
+        font-size: 1.5rem;
+        margin-top: 1rem;
+    `;
+    const Loading = styled(Italic)`
+        color: #ddd;
+    `;
+
     useEffect(() => {
         axios
-            .get('https://api.nasa.gov/planetary/apod?api_key=WkQiPbiZMW3bmRRzoV0rNN3bUZwbK0NbZv6hk6rg&date=2012-03-14')
+            .get('https://api.nasa.gov/planetary/apod?api_key=WkQiPbiZMW3bmRRzoV0rNN3bUZwbK0NbZv6hk6rg&date=2019-06-14')
             // .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`)
             .then(res => {
                 console.log("res:", res);
                 console.log("res.data:", res.data);
-                console.log("res.config.url:", res.config.url);
                 console.log("res.data.url: ", res.data.url);
-
                 setPhotos(res.data);
             })
             .catch(err => {
@@ -22,12 +65,15 @@ const NasaPictures = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Nasa Photo of the Day</h1>
-            <h3>{photos.title}</h3>
-            <img src={photos.url} className="photo" alt="nasa pic of the day" />
-            <p>{photos.explanation}</p>
-        </div>
+        <ContainerDiv>
+            <Title>Nasa Photo of the Day</Title>
+            <PhotoTitle>{photos.title}</PhotoTitle>
+            {!photos.url ? 
+                <h4>Loading...</h4> : 
+                <img src={photos.url} className="photo" alt="nasa pic of the day" />            
+            }
+            <Explanation>{photos.explanation}</Explanation>
+        </ContainerDiv>
     );
 };
 
